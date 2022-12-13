0b1125acf80859cc67e5f12d149bad89aa7ace64
xuan1440 <pp0938609151@gmail.com>
2022-12-13 12:08:07 +0800

    week15

---
+++ week15/week15-3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    printf("A的值是 %d\n", 'A');
+    printf("B的值是 %d\n", 'B');
+    printf("C的值是 %d\n", 'C');
+
+    for(int c = 'A'; c<='Z'; c++){
+        printf("%C 的值是 %d\n", c, c);
+    }
+}
+
